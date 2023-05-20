880c6d3e9ff115acbb5a6e5d70f35af1ba97ddd5
abu-jafor <abujaforwd@yahoo.com>
2023-05-20 10:50:32 +0600

    un nesesary data deleted

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const dataCollection = client.db("dataStore").collection("products");
 
